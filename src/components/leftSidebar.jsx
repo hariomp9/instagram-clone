ef586ad440117d73ece1dbced3941e65f85c9ad6
shubham21222 <shubhamraikwar@hotmail.com>
2024-10-18 14:49:19 +0530

    work on leftsidebar

--- src/components/leftSidebar.jsx
+++ src/components/leftSidebar.jsx
@@ -21,10 +21,7 @@ import { useNavigate } from "react-router-dom";
 import CreatePost from "./createPost";
 import Loader from "@/utils/loader";
 import { setPosts } from "@/redux/postSlice";
-
-import { Toaster } from "react-hot-toast";
 import CreateReel from "./CreateReel";
-
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { Button } from "./ui/button";
 import toast, { Toaster } from "react-hot-toast";
